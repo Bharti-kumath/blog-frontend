d3b2c22588b486aaae11b1feffe354df10a88a00
Bharti Kumath <77137277+Bharti-kumath@users.noreply.github.com>
2022-11-27 11:28:04 +0530

    Update Single.jsx

--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -2,7 +2,7 @@ import "./single.css";
 import Singlepost from "../../components/singlepost/Singlepost";
 import Footer from "../../components/Footer/footer";
 export default function Single() {
-  return <div className="single" style={{ backgroundColor: "#95cde2" }}>
+  return <div className="single" style={{ backgroundColor: "#62bbe6" }}>
   
   <Singlepost/>
     <Footer/>
